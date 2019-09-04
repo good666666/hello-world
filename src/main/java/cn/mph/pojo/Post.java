68645cecc52d2d65a7bc7df0ff40e0cf2e700d21
mph <951772645@qq.com>
2019-09-04 16:54:21 +0800

    第三个尼古拉斯·Then

---
+++ src/main/java/cn/mph/pojo/Post.java
@@ -0,0 +1,11 @@
+package cn.mph.pojo;
+
+/**
+ * 姓名: Java 孟
+ * 时间 : 2019/9/4 16:49
+ */
+public class Post {
+    public static void main(String[] args) {
+        System.out.println("尼古拉斯·赵四");
+    }
+}
